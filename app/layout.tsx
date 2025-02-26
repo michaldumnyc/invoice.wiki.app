eb9e10ba80c7d381623b0a905199e99e6974eb76
Sequorem s.r.o. <meshahatar2@gmail.com>
2025-02-26 17:39:23 +0100

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type React from "react";
 import { Inter } from "next/font/google";
-import "../styles/global.css";
+import "@/styles/global.css";
 import type { Metadata } from "next";
 import { headers } from "next/headers";
 import Script from "next/script";
