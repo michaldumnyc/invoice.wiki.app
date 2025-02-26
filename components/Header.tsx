23e5f667dc4dc1f8f53324dcad6461c9cc2420f0
Sequorem s.r.o. <meshahatar2@gmail.com>
2025-02-26 13:17:35 +0100

    Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,7 +12,6 @@ export default function Header() {
   return (
     <header className="fixed top-0 left-0 right-0 z-50 bg-white/80 backdrop-blur-sm border-b">
       <div className="w-full px-3 sm:px-4 py-4">
-        {/* Reduced padding and removed container class */}
         <nav className="flex justify-between items-center max-w-7xl mx-auto">
           <Link
             href="/"
@@ -21,8 +20,8 @@ export default function Header() {
             Invoice.wiki
           </Link>
 
-          {/* Desktop Navigation */}
-          <ul className="lg:flex space-x-6">
+          {/* Desktop & Tablet Navigation (Visible on lg and larger) */}
+          <ul className="hidden lg:flex space-x-6">
             <li>
               <Link
                 href="/about"
@@ -47,7 +46,7 @@ export default function Header() {
             </li>
           </ul>
 
-          {/* Mobile Menu */}
+          {/* Mobile Menu (Visible only on small screens) */}
           <MobileMenu />
         </nav>
       </div>
