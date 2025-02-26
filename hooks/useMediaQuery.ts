a9c6e6a8b277d33418074cc8f55f9731a2cd3c45
Sequorem s.r.o. <meshahatar2@gmail.com>
2025-02-26 14:26:19 +0100

    useMediaQuery.ts

--- hooks/useMediaQuery.ts
+++ hooks/useMediaQuery.ts
@@ -7,14 +7,16 @@ export function useMediaQuery(query: string): boolean {
 
   useEffect(() => {
     const media = window.matchMedia(query)
-    if (media.matches !== matches) {
-      setMatches(media.matches)
-    }
-    const listener = () => setMatches(media.matches)
-    media.addListener(listener)
-    return () => media.removeListener(listener)
-  }, [matches, query])
+
+    setMatches(media.matches)
+
+    const listener = (event: MediaQueryListEvent) => setMatches(event.matches)
+
+    media.addEventListener("change", listener)
+    return () => media.removeEventListener("change", listener)
+  }, [query])
 
   return matches
 }
+
 
