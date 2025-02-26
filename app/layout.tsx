e20363fe8d50c615b71148490593ab91eaef5f3f
Sequorem s.r.o. <meshahatar2@gmail.com>
2025-02-26 17:46:21 +0100

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
         {process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY && (
           <Script
             src={`https://www.google.com/recaptcha/api.js?render=${process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}`}
-            nonce={nonce}
+            nonce={nonce ?? undefined}
           />
         )}
       </body>
