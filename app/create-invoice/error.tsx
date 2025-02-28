db1df2ba58cdb4cf1a6678f973303f7a27e51371
Sequorem s.r.o. <meshahatar2@gmail.com>
2025-02-28 09:41:05 +0100

    error.tsx

--- app/create-invoice/error.tsx
+++ app/create-invoice/error.tsx
@@ -1,1 +1,22 @@
+"use client" 
+
+import { Alert, AlertDescription } from "@/components/ui/alert"
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error
+  reset: () => void
+}) {
+  return (
+    <Alert variant="destructive">
+      <AlertDescription>
+        <p>Oops. Something went wrong!</p>
+        <p>{error.message}</p>
+        <button onClick={() => reset()}>Try again</button>
+      </AlertDescription>
+    </Alert>
+  )
+}
 
