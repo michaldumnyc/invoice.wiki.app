bb0541eae09b2ef3fc828a8a928eebb9d3bb4bda
Michal D. <meshahatar2@gmail.com>
2025-04-23 16:57:18 +0200

    Update currencies.ts

--- app/utils/currencies.ts
+++ app/utils/currencies.ts
@@ -1,5 +1,5 @@
 export const currencies = [
   { code: "USD", name: "US Dollar", symbol: "$" },
   { code: "EUR", name: "Euro", symbol: "€" },
-]
-
+  { code: "GBP", name: "British Pound", symbol: "£" },
+];
